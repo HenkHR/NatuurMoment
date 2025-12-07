6e6afcdb32c58c9cb9597de11bd4568e3a1d363b
Quinten van der Vlies <1074726@hr.nl>
2025-12-08 00:54:04 +0100

    CHANGED: WCAG and responsiveness

--- resources/views/components/nav/home.blade.php
+++ resources/views/components/nav/home.blade.php
@@ -1,16 +1,10 @@
-@props([
-    'title' => 'Natuurmonumenten',
-    'back' => false,
-])
-
 <header class="bg-forest-700 shadow-card">
     <div class="max-w-5xl mx-auto h-16 px-4 lg:px-8 flex items-center gap-4">
-
-        <a href="{{ url('/') }}" class="flex items-center">
+        <a href="{{ url('/') }}" class="flex items-center h-full">
             <img
                 src="{{ asset('images/NM_LOGO.png') }}"
                 alt="Natuurmonumenten logo"
-                class="h-10 md:h-12 w-auto"
+                class="h-12 md:h-14 max-h-full w-auto"
             >
         </a>
 

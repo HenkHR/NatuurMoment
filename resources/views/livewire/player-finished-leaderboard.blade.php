d7fba2f8fbfac28eb686a46bbb9a863fb52d4724
Henk-Jan <1051620@hr.nl>
2025-12-10 22:01:20 +0100

    Dubbele leaderboard fixed

--- resources/views/livewire/player-finished-leaderboard.blade.php
+++ resources/views/livewire/player-finished-leaderboard.blade.php
@@ -10,13 +10,14 @@
     <!-- Content -->
     <section class="flex-1 w-full bg-white pt-8 px-4 pb-24 relative z-10">
 
-        <div class="w-full bg-[#f5f5f5] rounded-2xl px-4 py-6 shadow-sm">
+        <div class="w-full bg-[#f5f5f5] rounded-2xl px-4 py-6 shadow-sm max-w-md mx-auto">
 
             <div class="text-xs">
 
                 <x-leaderboard
                     :players="$leaderboardData"
                     :showContinueButton="true"
+                    :isFinished="true"
                 >
                     <button
                         wire:click="showFeedbackForm"
@@ -30,14 +31,4 @@
         </div>
 
     </section>
-
-    <div class="max-w-md mx-auto">
-        <x-leaderboard :players="$leaderboardData" :showContinueButton="true" :isFinished="true">
-            <button
-                wire:click="showFeedbackForm"
-                class="inline-block px-6 py-3 bg-[#2E7D32] hover:bg-green-700 text-white rounded-lg font-semibold transition">
-                Verder
-            </button>
-        </x-leaderboard>
-    </div>
 </div>
